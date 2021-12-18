47c09eda1dc52ee63a6a8267ea309d06e5ef5200
Omkar Kadam <itsoftware3@stulzservice.in>
2021-12-18 11:33:10 +0530

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -9,6 +10,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
