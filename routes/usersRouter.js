d6784691d327ffc3586d12c207d6788bd33ea8e2
Classic95 <omkar.kadam.2910@gmail.com>
2021-12-19 19:40:24 +0530

    updated code for file upload

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,11 +2,28 @@ var express = require('express');
 var router = express.Router();
 const userscontroller = require("../controllers/userController");
 const auth = require("../middleware/auth");
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // console.log('file----', file)
+    cb(null, 'views')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + path.extname(file.originalname)
+    console.log('file----', file, uniqueSuffix)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 /* GET users listing. */
 router.get('/list', auth.verifyToken, userscontroller.getUsersList);
+router.get('/graph', auth.verifyToken, userscontroller.getUsersGraph);
 router.get('/:id', auth.verifyToken, userscontroller.getUserProfile);
-router.post('/', auth.verifyToken, userscontroller.addUsersList);
-router.patch('/:id', auth.verifyToken, userscontroller.updateUsersList);
+router.post('/', auth.verifyToken, upload.single('avatar') , userscontroller.addUsers);
+router.patch('/:id', auth.verifyToken, upload.single('avatar'), userscontroller.updateUsers);
 
 module.exports = router;
